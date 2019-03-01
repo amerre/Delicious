fd47bd74aad2cc7e47858323c8b09dbbc1ba0191
amerre <adr.merre@gmail.com>
2019-03-01 14:14:43 +0100

    :contruction: Testing routing stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const adri = { name: "Adri", age: 21, cool: true };
+  // res.send("Hey! It works!");
+  // res.json(adri);
+  // res.send(req.query.name);
+  res.json(req.query);
+});
+
+router.get("/reverse/:name", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;
